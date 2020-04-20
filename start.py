db6dfb6b37fcb36df1d246fd7bbb38db3f06903a
candra2802 <appletjandra@gmail.com>
2020-04-20 08:56:18 +0700

    Add files via upload

--- start.py
+++ start.py
@@ -1,8 +1,8 @@
-from mnist import MNIST
 import numpy as np
 from lib import *
+from mnist.loader import MNIST
 
-mndata = MNIST('.data')
+mndata = MNIST('./data')
 images, labels = mndata.load_training()
 t_images, t_labels = mndata.load_testing()
 
